506c545d766b3a7899bd32875f5dd933db4c1796
walking-octopus <46994949+walking-octopus@users.noreply.github.com>
2024-12-02 22:03:54 +0200

    Day 2

---
+++ day-2.scm
@@ -0,0 +1,39 @@
+(require srfi/1)
+
+(define (read-input path)
+    (map (λ (line)
+            (map string->number
+                 (string-split line " ")))
+         (file->lines path)))
+
+(define (diff xs)
+  (map - (cdr xs) xs))
+
+(define (monotonic? xs)
+  (or (andmap positive? xs)
+      (andmap negative? xs)))
+
+(define (safe? report)
+    (let ([ds (diff report)])
+      (and (monotonic? ds)
+           (andmap (λ (n) (<= 1 (abs n) 3))
+                   ds))))
+
+(define (part-1 reports)
+  (count safe? reports))
+
+(define (remove-at-index id xs)
+    (append (take xs id)
+            (drop xs (+ 1 id))))
+
+(define (safe-with-removal? report)
+  (define (try-remove i)
+    (and (< i (length report))
+         (or (safe? (remove-at-index i report))
+             (try-remove (+ i 1)))))
+  (or (safe? report)
+      (try-remove 0)))
+
+(let ([reports (read-input "Downloads/input"))]
+     (println (count safe? reports))
+     (println (count safe-with-removal? reports)))
